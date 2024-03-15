b601eb81cd500c30a2ff48993b95282037e29ea3
Wolfyxon <wolfyxon@gmail.com>
2024-03-15 21:16:33 +0100

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# MinVideo
+A simple video format easy to adapt in limited environments.
+
+## How it works
+`16` bytes are reserved for the size. Each dimension is `8` bytes so the maximum video size is `2040x2040`. However this can be easily changed by modifying the code, but this will make the decoder not being able to properly parse videos made in the regular format.
+
+After that there are the frames. Each frames consists of `width * height` bytes which hold the colors. 
+
+Each color is `3` bytes: red, green, blue. Colors can hold numbers up to 255, example `[255, 0, 0]` is red.
+
+I hope this will help you develop your own decoder or encoder following this format, for your project.
+
+## Why?
+It's a very raw format without any compression or complicated algorithms which makes it usable in example electronics emulators or games. You can make a circuit for rendering videos then encode the bytes to a ROM which the circuit will use.
+
+You can also use it in game engines that don't support video playback. You would need to read a file's bytes or convert a string to bytes if file access is not possible. You can also use the data numbers, but it will negatively impact the file size.
---
+++ python/min_video.py
@@ -0,0 +1,150 @@
+import math
+
+VIDEO_SIZE_BYTE_LENGTH = 8
+VIDEO_MAX_DIMENSION = VIDEO_SIZE_BYTE_LENGTH * 255
+BYTES_BEFORE_FRAMES = VIDEO_SIZE_BYTE_LENGTH * 2
+
+def dimension_split(dimension: int) -> list[int]:
+    res = []
+
+    if dimension != 0:
+        count = math.ceil(dimension / 255)
+        res = [dimension // count] * count
+
+        for i in range(dimension % count):
+            res[i] += 1
+
+
+    while len(res) < VIDEO_SIZE_BYTE_LENGTH:
+        res.append(0)
+
+    return res
+
+def get_coords_at_idx(frame_index: int, width: int, height: int) -> tuple[int, int]:
+    x = frame_index % width
+    y = (frame_index // width) % height
+
+    return x, y
+
+
+class Frame:
+    def __init__(self, width, height) -> None:
+        self.pixels: list[ list[int] ] = []
+        self.width = width
+        self.height = height
+
+        for i in range(width * height):
+            self.pixels.append( [0, 0, 0] )
+
+    def get_area(self) -> int:
+        return self.width * self.height
+
+    def get_index(self, x: int, y: int) -> int:
+        return y * self.width + x
+
+    def set_color(self, x: int, y: int, r: int, g: int, b: int):
+        idx = self.get_index(x, y)
+
+        if idx >= len(self.pixels):
+            raise ValueError("Pixel out of range")
+
+        px = [r, g, b]
+        self.pixels[idx] = px
+
+    def get_color(self, x: int, y: int) -> list[int]:
+        idx = self.get_index(x, y)
+
+        if idx >= len(self.pixels):
+            return
+
+        return self.pixels[idx]
+
+    def get_data(self) -> list[int]:
+        res = []
+
+        for i in self.pixels:
+            for c in i:
+                res.append(c)
+
+        return res
+
+class Video:
+    def __init__(self, width: int, height: int) -> None:
+        self.frames: list[Frame] = []
+        self.width = width
+        self.height = height
+
+    def add_frame(self, frame: Frame):
+        self.frames.append(frame)
+
+    def get_area(self) -> int:
+        return self.width * self.height
+
+    def get_data(self) -> list[int]:
+        data = []
+
+        for i in dimension_split(self.width):
+            data.append(i)
+
+        for i in dimension_split(self.height):
+            data.append(i)
+
+        for frame in self.frames:
+            for color in frame.get_data():
+                data.append(color)
+
+        return data
+
+    def get_bytes(self) -> str:
+        return bytes(self.get_data())
+
+    def save_file(self, path: str):
+        with open(path, "wb") as file:
+            file.write(self.get_bytes())
+
+    @staticmethod
+    def get_width_from_data(data: list[int]) -> int:
+        return sum(data[:VIDEO_SIZE_BYTE_LENGTH])
+
+    @staticmethod
+    def get_height_from_data(data: list[int]) -> int:
+        return sum(data[VIDEO_SIZE_BYTE_LENGTH:VIDEO_SIZE_BYTE_LENGTH * 2])
+
+    @staticmethod
+    def get_frame_amount_from_data(data: list[int]) -> int:
+        w = Video.get_width_from_data(data)
+        h = Video.get_height_from_data(data)
+
+        return ( len(data) - VIDEO_SIZE_BYTE_LENGTH * 2) // 3 // (w * h)
+
+    @staticmethod
+    def from_data(data: list[int]):
+        data_len = len(data)
+        w = Video.get_width_from_data(data)
+        h = Video.get_height_from_data(data)
+        pixel_amt = w * h * 3
+        vid = Video(w, h)
+        frames = (data_len - VIDEO_SIZE_BYTE_LENGTH * 2) // pixel_amt
+
+        for frame_i in range(frames):
+            frame = Frame(w, h)
+
+            for i in range(pixel_amt):
+                x, y = get_coords_at_idx(i, w, h)
+
+                # Ensure we don't go beyond the end of the data
+                if frame_i * pixel_amt + i + BYTES_BEFORE_FRAMES + 2 < data_len:
+                    r = data[frame_i * pixel_amt + i + BYTES_BEFORE_FRAMES]
+                    g = data[frame_i * pixel_amt + i + BYTES_BEFORE_FRAMES + 1]
+                    b = data[frame_i * pixel_amt + i + BYTES_BEFORE_FRAMES + 2]
+
+                    frame.set_color(x, y, r, g, b)
+
+            vid.add_frame(frame)
+
+        return vid
+
+    @staticmethod
+    def from_file(path: str):
+        with open(path, "rb") as file:
+            return Video.from_data( file.read() )
