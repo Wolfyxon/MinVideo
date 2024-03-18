0961ad4980688e779bc69a434d53893fc358654a
Wolfyxon <wolfyxon@gmail.com>
2024-03-18 08:39:51 +0100

    Added parse option

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,10 @@
 mod min_video;
+use min_video::Video;
+
 use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
 
 struct Option<'a> {
     alias: &'a str,
@@ -14,6 +19,12 @@ fn main() {
             alias: "help",
             callback: help_option,
             minimum_args: 0
+        },
+
+        Option {
+            alias: "parse",
+            callback: parse_option,
+            minimum_args: 1
         }
     ];
 
@@ -51,4 +62,20 @@ fn main() {
 fn help_option(_args: Vec<String>) {
     println!("MinVideo command line tool");
     println!("Source: https://github.com/Wolfyxon/MinVideo");
+}
+
+fn parse_option(args: Vec<String>) {
+    let path = args[0].to_string();
+
+    let mut file = File::open(&path).expect(format!("error: File {} not found", path).as_str());
+    let metadata = fs::metadata(&path).expect("error: unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    file.read(&mut buffer).expect("error: buffer overflow");
+
+    let w = Video::get_width_from_data(&buffer);
+    let h = Video::get_height_from_data(&buffer);
+    let frames = Video::get_frame_amount_from_data(&buffer);
+
+    println!("Size: {}x{}", w, h);
+    println!("Frames: {}", frames);
 }
