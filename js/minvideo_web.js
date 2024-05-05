efd2719faa4b688d6b5ed2f01772bd7fce2ef963
Wolfyxon <wolfyxon@gmail.com>
2024-05-05 12:15:16 +0200

    Added dimensionSplit()

--- js/minvideo_web.js
+++ js/minvideo_web.js
@@ -7,3 +7,26 @@ minvideo.VIDEO_SIZE_BYTE_LENGTH = 8
 minvideo.VIDEO_MAX_DIMENSION = VIDEO_SIZE_BYTE_LENGTH * 255
 minvideo.BYTES_BEFORE_FRAMES = VIDEO_SIZE_BYTE_LENGTH * 2
 
+/**
+ * @param {number} dimension
+ * @returns {number[]} 
+ */
+minvideo.dimensionSplit = function(dimension){
+    const res = [];
+
+    if(dimension !== 0) {
+        const count = Math.ceil(dimension / 255);
+
+        for(let i = 0; i < count; i++ ) {
+            res.push(dimension / count);
+        }
+
+        for(let i = 0; i < dimension % count; i++) {
+            res[i] += 1;
+        }
+    }
+
+    while(res.length < minvideo.VIDEO_SIZE_BYTE_LENGTH) {
+        res.push(0);
+    }
+}
