c1f5abb54067296e9401e4420c79cf980e1c6121
Wolfyxon <wolfyxon@gmail.com>
2024-05-05 12:15:04 +0200

    Init JavaScript version

---
+++ js/minvideo_web.js
@@ -0,0 +1,9 @@
+// JavaScript MinVideo version for web browsers
+// Accessed by importing the script, then using the functions and classes in other scripts
+
+const minvideo = {};
+
+minvideo.VIDEO_SIZE_BYTE_LENGTH = 8
+minvideo.VIDEO_MAX_DIMENSION = VIDEO_SIZE_BYTE_LENGTH * 255
+minvideo.BYTES_BEFORE_FRAMES = VIDEO_SIZE_BYTE_LENGTH * 2
+
