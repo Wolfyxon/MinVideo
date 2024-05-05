18063302fca0ee2b198c433d62e6ccaf80efefda
Wolfyxon <wolfyxon@gmail.com>
2024-05-05 12:18:11 +0200

    Fixed undefined values

--- js/minvideo_web.js
+++ js/minvideo_web.js
@@ -4,8 +4,8 @@
 const minvideo = {};
 
 minvideo.VIDEO_SIZE_BYTE_LENGTH = 8;
-minvideo.VIDEO_MAX_DIMENSION = VIDEO_SIZE_BYTE_LENGTH * 255;
-minvideo.BYTES_BEFORE_FRAMES = VIDEO_SIZE_BYTE_LENGTH * 2;
+minvideo.VIDEO_MAX_DIMENSION = minvideo.VIDEO_SIZE_BYTE_LENGTH * 255;
+minvideo.BYTES_BEFORE_FRAMES = minvideo.VIDEO_SIZE_BYTE_LENGTH * 2;
 
 /**
  * @param {number} dimension
