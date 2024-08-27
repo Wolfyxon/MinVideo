0b212ab55bbdf88e29564fcf5a4e97bc653c8fbb
Wolfyxon <wolfyxon@gmail.com>
2024-08-28 00:36:04 +0200

    Added MinVideoFrame struct

--- c/minvideo.h
+++ c/minvideo.h
@@ -1,6 +1,13 @@
 #ifndef minvideo_h__
 #define minvideo_h__
 
+struct MinVideoFrame {
+    int data_len;
+    int data;
+    int width;
+    int height;
+};
+
 extern int minvideo_get_idx_at_coords(int x, int y, int width);
 
 #endif
