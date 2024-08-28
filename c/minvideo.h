e93ff7a23cf25b6db7d96abc3aba70577fa0fded
Wolfyxon <wolfyxon@gmail.com>
2024-08-28 10:37:03 +0200

    Better struct style

--- c/minvideo.h
+++ c/minvideo.h
@@ -5,20 +5,18 @@ extern const unsigned int VIDEO_SIZE_BYTE_LENGTH;
 extern const unsigned int VIDEO_MAX_DIMENSION;
 extern const unsigned int BYTES_BEFORE_FRAMES;
 
-typedef struct MinVideoFrame MinVideoFrame;
-struct MinVideoFrame {
+typedef struct {
     unsigned int data_len;
     unsigned int *data;
     unsigned int width;
     unsigned int height;
-};
+} MinVideoFrame;
 
-typedef struct MinVideoColor MinVideoColor;
-struct MinVideoColor {
+typedef struct {
     unsigned int r;
     unsigned int g;
     unsigned int b;
-};
+} MinVideoColor;
 
 extern void MinVideoFrame_set_color(MinVideoFrame* frame, unsigned int x, unsigned int y, MinVideoColor color);
 extern MinVideoColor MinVideoFrame_get_color(MinVideoFrame* frame, unsigned int x, unsigned int y);
