7073afba0dbe1a8dfbf16a5c4cdb3ea253b0df47
Wolfyxon <wolfyxon@gmail.com>
2024-08-28 10:36:17 +0200

    Implemented MinVideoFrame_get_color()

--- c/minvideo.c
+++ c/minvideo.c
@@ -4,6 +4,18 @@ const unsigned int VIDEO_SIZE_BYTE_LENGTH = 0;
 const unsigned int VIDEO_MAX_DIMENSION = VIDEO_SIZE_BYTE_LENGTH * 255;
 const unsigned int BYTES_BEFORE_FRAMES = VIDEO_SIZE_BYTE_LENGTH * 2;
 
+ MinVideoColor MinVideoFrame_get_color(MinVideoFrame* frame, unsigned int x, unsigned int y) {
+    unsigned int begin = minvideo_get_idx_at_coords(x, y, frame->width) * 3;
+
+    MinVideoColor color;
+
+    color.r = frame->data[begin + 0];
+    color.g = frame->data[begin + 1];
+    color.b = frame->data[begin + 2];
+    
+    return color;
+ }
+
 unsigned int minvideo_get_idx_at_coords(unsigned int x, unsigned int y, unsigned int width) {
     return y * width + x;
 }
