476efa76b956e182b990594aa2a2e6505409137d
Wolfyxon <wolfyxon@gmail.com>
2024-08-28 10:38:05 +0200

    Removed whitespace

--- c/minvideo.c
+++ c/minvideo.c
@@ -4,7 +4,7 @@ const unsigned int VIDEO_SIZE_BYTE_LENGTH = 0;
 const unsigned int VIDEO_MAX_DIMENSION = VIDEO_SIZE_BYTE_LENGTH * 255;
 const unsigned int BYTES_BEFORE_FRAMES = VIDEO_SIZE_BYTE_LENGTH * 2;
 
- MinVideoColor MinVideoFrame_get_color(MinVideoFrame* frame, unsigned int x, unsigned int y) {
+MinVideoColor MinVideoFrame_get_color(MinVideoFrame* frame, unsigned int x, unsigned int y) {
     unsigned int begin = minvideo_get_idx_at_coords(x, y, frame->width) * 3;
 
     MinVideoColor color;
